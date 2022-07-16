cedc7de7d2a96db361d3bc52d4efa922b0f5865e
KIPASGTS <83524667+KIPASGTS@users.noreply.github.com>
2022-07-17 06:49:12 +0700

    Update bussid.py

--- bussid.py
+++ bussid.py
@@ -1,49 +1,54 @@
-#NOTE: JUST FOR FUN XD
-
-import requests, json, time, threading
+import requests, json, time, threading, os
+record = [{'Key': {'sourceCity': 'MLG', 'destinationCity': 'SBY', 'routePassed': ['SBY', 'MLG'], 'activityRewards': None}, 'Value': 30}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'SBY'], 'activityRewards': None}, 'Value': 60}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'SMG', 'routePassed': ['SMG', 'MLG'], 'activityRewards': None}, 'Value': 12}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SMG'], 'activityRewards': None}, 'Value': 50}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'SBY'], 'activityRewards': None}, 'Value': 10}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'CBN', 'routePassed': ['CBN', 'MLG'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'CBN', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'CBN'], 'activityRewards': None}, 'Value': 45}, {'Key': {'sourceCity': 'SMG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SMG'], 'activityRewards': None}, 'Value': 9}, {'Key': {'sourceCity': 'SBY', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'SBY'], 'activityRewards': None}, 'Value': 5}, {'Key': {'sourceCity': 'MLG', 'destinationCity': 'JKT', 'routePassed': ['JKT', 'MLG'], 'activityRewards': None}, 'Value': 3}, {'Key': {'sourceCity': 'JKT', 'destinationCity': 'P_Merak', 'routePassed': ['P_Merak', 'JKT'], 'activityRewards': None}, 'Value': 45}]
+auth = input('Auth: ')
 headers = {'User-Agent': 'UnityEngine-Unity; Version: 2018.4.26f1','X-ReportErrorAsSuccess': 'true','X-PlayFabSDK': 'UnitySDK-2.20.170411','X-Authorization': '','Content-Type': 'application/json','Content-Length': '157','Host': '4ae9.playfabapi.com'}
-
+headers['X-Authorization'] = auth
+counter = 0
 def create_mission():
-	try:
-		data = '{"FunctionName":"PlayCareer","FunctionParameter":{"cities":["MLG","SBY","SMG","CBN","JKT","P_Merak","P_Bakauheni","LPG","PLB","JMB","PBR"]},"RevisionSelection":"Live","SpecificRevision":null,"GeneratePlayStreamEvent":false}'
-		response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=data).text
-		js = json.loads(response)
-		csession = js['data']['FunctionResult']['careerSession']
-		return csession
-	except Exception as e:
-		pass
-	
+	game_data = '{"FunctionName":"PlayCareer","FunctionParameter":{"cities":["MLG","SBY","SMG","CBN","JKT","P_Merak","P_Bakauheni","LPG","PLB","JMB","PBR"]},"RevisionSelection":"Live","SpecificRevision":null,"GeneratePlayStreamEvent":false}'
+	response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=game_data).text
+	if response != '':
+		parser = json.loads(response)
+		if parser['code'] == 401:
+			return None
+		elif parser['code'] == 200:
+			data = parser['data']
+			if "apiError" in str(data):
+				return None
+			else:
+				carrer = data['FunctionResult']['careerSession']
+				return carrer
+	else:
+		return None
 
-def skip_mission(token, rec):
-		try:
-			data = json.dumps({"FunctionName":"FarePayment","FunctionParameter":{"records":rec,"bonus":True,"careerToken":token,"activityRewardToken":"{\"rewards\":[]}"},"RevisionSelection":"Live","SpecificRevision":None,"GeneratePlayStreamEvent":False})
-			response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=data).text
-				
-			js = json.loads(response)
-			logs = js['data']['Logs']
-			cash = logs[len(logs)-1]['Message'].split()[5]
-			print(f'Total Balanced -> {cash}')
-		except Exception as e:
+def skip_mission(token):
+	data = json.dumps({"FunctionName":"FarePayment","FunctionParameter":{"records":record,"bonus":True,"careerToken":token,"activityRewardToken":"{\"rewards\":[]}"},"RevisionSelection":"Live","SpecificRevision":None,"GeneratePlayStreamEvent":False})
+	response = requests.post('https://4ae9.playfabapi.com/Client/ExecuteCloudScript', headers=headers, data=data).text
+	if response != '':
+		parser = json.loads(response)
+		if parser['code'] == 401:
 			pass
-			
+		elif parser['code'] == 200:
+			backend_data = parser['data']
+			if "apiError" in str(backend_data):
+				pass
+			else:
+				logs = backend_data['Logs']
+				cash = logs[len(logs)-1]['Message'].split()[5]
+				print(f'[{token}] Added {cash} balance to your account')
+	
 def pass_mission():
-	try:
-		csession = create_mission()
-		token = csession['token']
-		kota = ["MLG","SBY","SMG","CBN","JKT","P_Merak","P_Bakauheni","LPG","PLB","JMB","PBR"]
-		rec = []
-		for i in range(0, len(kota)):
-			passenger = csession['passenger'][i]
-			source = passenger['source']
-			dest = passenger['destination']
-			max_passenger = passenger['amount']
-			rec.append({"Key":{"sourceCity":source,"destinationCity":dest,"routePassed":[dest,source],"activityRewards":None},"Value":int(max_passenger)})
-		skip_mission(token, rec)
-	except Exception as e:
-		pass
+	carrer = create_mission()
+	if carrer != None:	
+		token = carrer['token']
+		skip_mission(token)
 
-auth = input('Auth token: ')
-headers['X-Authorization'] = auth	
-while True:
+
+while 1:
 	pass_mission()
-	time.sleep(4)
+	if counter == 3:
+		counter = 0
+		print('Sleeping 3.5 second prevent anti spam')
+		time.sleep(3.5)
+	else:
+		counter += 1
